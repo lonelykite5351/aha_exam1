1101be7cdc8e2db7e880fa988c221a8ccf05d8aa
lonelykite5351 <55124083+lonelykite5351@users.noreply.github.com>
2023-05-31 17:31:45 +0800

    PasswordInput add

---
+++ src/PasswordInput.js
@@ -0,0 +1,16 @@
+import './PasswordInput.css';
+
+const PasswordInput = () => {
+  return (
+    <div className="PasswordInput w-[1219px] h-[1214px] flex flex-col">
+      <div className="top bg-[#181818] py-[70px] pl-[122px] rounded-t-[35px]">
+        <h1 className="h1Title">Password Input</h1>
+      </div>
+      <div className="bottom">
+        <h1>bottom</h1>
+      </div>
+    </div>
+  );
+};
+
+export default PasswordInput;
